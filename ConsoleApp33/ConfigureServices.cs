7fb6ebea22d945c9431a6154f19073c80d1564f7
LukaszSzulc <lukasz.schultz@gmail.com>
2019-06-23 21:31:00 +0200

    Add project files.

---
+++ ConsoleApp33/ConfigureServices.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace ConsoleApp33
+{
+
+    public static class ConfigureServices
+    {
+        public static ServiceProvider Create()
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false).Build();
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddOptions();
+            serviceCollection.AddLogging(logger =>
+            {
+                logger.AddConsole();
+            });
+
+            serviceCollection.AddTransient<Program>();
+            return serviceCollection.BuildServiceProvider();
+        }
+    }
+}
---
+++ ConsoleApp33/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp33
+{
+    class Program
+    {
+        private readonly ILogger<Program> logger;
+
+        public Program(ILogger<Program> logger)
+        {
+            this.logger = logger;
+        }
+        static async Task Main(string[] args)
+        {
+            var services = ConfigureServices.Create();
+            var application = services.GetRequiredService<Program>();
+            await application.RunAsync();
+        }
+
+        public Task RunAsync()
+        {
+            logger.LogInformation("Hello");
+            logger.LogInformation("Test");
+            logger.LogInformation("Finished");
+            return Task.CompletedTask;
+        }
+    }
+}
