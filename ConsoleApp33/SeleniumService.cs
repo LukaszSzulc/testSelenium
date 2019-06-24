c971ebb3d043c791ffb2aba5d9f621cf88912a8e
LukaszSzulc <lukasz.schultz@gmail.com>
2019-06-24 16:14:54 +0200

    next

--- ConsoleApp33/SeleniumService.cs
+++ ConsoleApp33/SeleniumService.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Chrome;
 using Microsoft.Extensions.Logging;
+using System.Threading;
 
 namespace ConsoleApp33
 {
@@ -14,24 +15,38 @@ namespace ConsoleApp33
     {
         private readonly IOptions<SeleniumConfiguration> options;
         private readonly ILogger<SeleniumService> logger;
+        private readonly IDomainProvider domainServiceProvider;
+        private readonly IAzureStorage azureStorage;
 
-        public SeleniumService(IOptions<SeleniumConfiguration> options, ILogger<SeleniumService> logger)
+        public SeleniumService(IOptions<SeleniumConfiguration> options, ILogger<SeleniumService> logger, IDomainProvider domainServiceProvider, IAzureStorage azureStorage)
         {
             this.options = options;
             this.logger = logger;
+            this.domainServiceProvider = domainServiceProvider;
+            this.azureStorage = azureStorage;
         }
 
-        public Task RunAsync()
+        public async Task RunAsync()
         {
             var chromeOptions = new ChromeOptions();
             chromeOptions.AddArgument("--headless");
-            using (var driver = new RemoteWebDriver(new Uri(options.Value.Url), chromeOptions))
+            var domains = await domainServiceProvider.Get();
+            int counter = 1;
+            object lockObject = new object();
+            Parallel.ForEach(domains, parallelOptions: new ParallelOptions { MaxDegreeOfParallelism = 16 }, async (domain) =>
             {
-                driver.Navigate().GoToUrl("https://google.com");
-                logger.LogInformation(driver.Title);
-            }
-
-            return Task.CompletedTask;
+                using (var driver = new RemoteWebDriver(new Uri(options.Value.Url), chromeOptions))
+                {
+                    driver.Navigate().GoToUrl($"http://{domain}");
+                    var screenshoot = driver.GetScreenshot();
+                    await azureStorage.Put(screenshoot.AsByteArray, domain);
+                    lock (lockObject)
+                    {
+                        logger.LogInformation($"processed {counter}/{domains.Count}");
+                        Interlocked.Increment(ref counter);
+                    }
+                }
+            });
         }
     }
 }
