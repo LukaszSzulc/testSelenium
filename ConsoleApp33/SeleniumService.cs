27550211ac9d2cd93eb96f8a5e5acaccfa5f3d9d
LukaszSzulc <lukasz.schultz@gmail.com>
2019-06-24 13:56:57 +0200

    Commit

--- ConsoleApp33/SeleniumService.cs
+++ ConsoleApp33/SeleniumService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Chrome;
+using Microsoft.Extensions.Logging;
 
 namespace ConsoleApp33
 {
@@ -12,10 +13,12 @@ namespace ConsoleApp33
     public class SeleniumService : ISeleniumService
     {
         private readonly IOptions<SeleniumConfiguration> options;
+        private readonly ILogger<SeleniumService> logger;
 
-        public SeleniumService(IOptions<SeleniumConfiguration> options)
+        public SeleniumService(IOptions<SeleniumConfiguration> options, ILogger<SeleniumService> logger)
         {
             this.options = options;
+            this.logger = logger;
         }
 
         public Task RunAsync()
@@ -24,6 +27,8 @@ namespace ConsoleApp33
             chromeOptions.AddArgument("--headless");
             using (var driver = new RemoteWebDriver(new Uri(options.Value.Url), chromeOptions))
             {
+                driver.Navigate().GoToUrl("https://google.com");
+                logger.LogInformation(driver.Title);
             }
 
             return Task.CompletedTask;
